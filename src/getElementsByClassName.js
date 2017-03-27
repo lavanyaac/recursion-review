8a9a64d49ea95450279bfc30a3838502a72b8b2d
Hack Reactor Student <student@hackreactor.com>
2017-03-27 15:10:50 -0700

    Got getElementsByClassName up and going - order matters!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,39 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// document.body, element.childNodes, and element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var memo = [];
+  // debugger;
+
+  function getElements(elem){
+    testThisElem(elem);
+    
+    if ( elem.hasChildNodes() ){
+      var kids = elem.childNodes;
+      for(var i = 0 ; i<kids.length; i++){
+        getElements(kids[i]);
+      }
+    }
+
+  }
+
+  function testThisElem(thing){
+  		var elemClassList = thing.classList;
+      if( ( elemClassList !== undefined ) && elemClassList.contains(className) ) {
+	  	  memo = memo.concat(thing);
+      }
+      return;
+  }
+
+  getElements(document.body);
+// get an element
+// does it have children
+// if so recurse   memo.concat(getElementsByClassName)
+// if no children, does it have any classes
+// if so return it to an added memo;
+return memo;
+
 };
