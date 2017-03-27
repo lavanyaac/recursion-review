ed0db14af694247ecbfc6b69ab1b53e2c7b238b9
Hack Reactor Student <student@hackreactor.com>
2017-03-27 16:28:48 -0700

    Work in Progress on StringifyJSON. Dealing with undefined\functions...

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var memo = "";
+
+  if(Array.isArray(obj)){  // Array 
+  	var result = "[";
+  	var elements = obj.reduce(function(retVal, item){
+  		retVal += stringifyJSON(item);
+  		retVal +=",";
+  		return retVal;
+  	},"");
+  	result += elements;
+  	if(result === "[") { result += "]"; }
+  	memo += result.slice(0,-1).concat("]");
+
+  } else {
+
+  	var x = typeof(obj);
+  	switch(x){
+  		case("string"):
+  		 memo+= '\"'.concat(String(obj),'\"');
+  		 break;
+  		case("boolean"):
+		case("number"):
+		  memo += String(obj);
+		  break;
+  		default:
+  		memo += objectHelper(obj);
+  	}
+  }
+
+  function objectHelper(obj){
+  	if (obj === null){ return "null"; }
+  	var result = "{";
+  	for(var key in obj){
+  		if ( typeof(obj[key]) === "undefined" || typeof(obj[key]) === "function"){
+  			return;
+  		}
+  		var keyVal = "\"".concat(String(key),"\":");
+  		var val  = stringifyJSON(obj[key]);
+  		result = result.concat(keyVal, val, ",");
+  	}
+  	if(result === "{") { result += "}"; }
+  	result = result.slice(0,-1).concat("}");
+  	return result;
+  }
+
+  return memo;
 };
